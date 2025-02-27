10e2c5d7efea1f7d0a765d1cb72a0ad72eb16f22
Atla <30910804+Atla-Dev@users.noreply.github.com>
2025-02-27 19:55:45 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const mainDisplay = document.getElementById('#screen');
+
+const clearbutton = document.querySelector('#clearButton');
+
+const deleteButton = document.querySelector('#deleteButton');
+
+const decimalButton = document.querySelector('#decimalButton');
+
+const divideButton = document.querySelector('#divideButton');
+
+const sevenButton = document.querySelector('#sevenButton');
+
+const eightButton = document.querySelector('#eightButton');
+
+const nineButton = document.querySelector('#nineButton');
+
+const multiplyButton= document.querySelector('#multiplyButton');
+
+const fourButton = document.querySelector('#fourButton');
+
+const fiveButton = document.querySelector('#fiveButton');
+
+const sixButton = document.querySelector('#sixButton');
+
+const subtractButton = document.querySelector('#subtractButton');
+
+const oneButton = document.querySelector('#oneButton');
+
+const twoButton = document.querySelector('#twoButton');
+
+const threeButton = document.querySelector('#threeButton');
+
+const additionButton = document.querySelector('#additionButton');
+
+const doubleZeroButton = document.querySelector('#doubleZeroButton');
+
+const zeroButton = document.querySelector('#zeroButton');
+
+const equalsButton = document.querySelector('#equalsButton');
+
+
+function addChar(val) {
+    document.getElementById("mainDisplay").value += val
+}
+
+
+
+
+
